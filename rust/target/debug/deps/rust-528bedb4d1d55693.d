6001748f0c3a9bdc1619487d64cdf204b7225b6f
saurav123anand <123sauravanand@gmail.com>
2024-01-15 02:57:20 +0530

    hello world added

---
+++ rust/target/debug/deps/rust-528bedb4d1d55693.d
@@ -0,0 +1,5 @@
+/Users/SA20408505/Documents/rustprogramming/RustProgramming/rust/target/debug/deps/rust-528bedb4d1d55693: src/main.rs
+
+/Users/SA20408505/Documents/rustprogramming/RustProgramming/rust/target/debug/deps/rust-528bedb4d1d55693.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/Users/SA20408505/Documents/rustprogramming/RustProgramming/rust/target/debug/rust: /Users/SA20408505/Documents/rustprogramming/RustProgramming/rust/src/main.rs
