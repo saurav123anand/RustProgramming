ac38821f90d6867c4e31e8895727f8aecfe2c65b
saurav123anand <123sauravanand@gmail.com>
2024-01-15 12:55:37 +0530

    guessing game

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,17 @@
+// fn main(){
+//     // println!("hello world")
+//     let mut apples=5;
+//     apples=7;
+//     println!("{apples}");
+// }
+
+// guessing game 
+
+use std::io;
 fn main(){
-    println!("hello world")
+    println!("Guess the number !");
+    println!("Please input your guess.");
+    let mut guess=String::new();
+    io::stdin().read_line(&mut guess).expect("failed to read line.");
+    println!("You guessed: {guess}");
 }
